8fbcb24932ba3ebfeb5a07bea7bf7b2b6977d683
Eloise Taylor <rockspud5693@gmail.com>
2021-03-14 11:21:06 +0000

    Caching dependency data

--- src/server/get-npm-dependencies.js
+++ src/server/get-npm-dependencies.js
@@ -1,15 +1,31 @@
 const getDeps = require('get-dependencies');
+Cache = require('cache');
+
+const oneMinuteInMs = 60000;
+const cacheTtl = oneMinuteInMs * 60;
 
 exports.GetDependencies = async (packageName) => {
-    const dependencies = await getDeps.getByName(packageName)
-        .then((result) => {
-            return result;
-        })
-        .catch(err => { console.log(err.message); });
+    
+    const dependencyCache = new Cache(cacheTtl, 'cache-data.json');
+
+    let dependencies = dependencyCache.get(packageName);
+
+    if (dependencies) {
+        console.log('Got dependencies from cache');
+    } else {
+        console.log('Getting dependencies from API');
+
+        dependencies = await getDeps.getByName(packageName)
+            .then((result) => {
+                return result;
+            })
+            .catch(err => { console.log(err.message); });
+
+        dependencyCache.put(packageName, dependencies);
+    }
 
     //let treeView = dependencies;
     // console.log(treeView);
-    //cache each call
 
     return dependencies;
 };
