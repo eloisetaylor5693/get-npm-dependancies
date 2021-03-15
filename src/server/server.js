9851ce50db55252e7d2ba287c0dd3b32fb93bc5c
Eloise Taylor <rockspud5693@gmail.com>
2021-03-15 12:56:44 +0000

    Returning error response

--- src/server/server.js
+++ src/server/server.js
@@ -15,18 +15,26 @@ const server = http.createServer(async function (req, res) {
     }
     console.log(packageName);
 
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    });
-
-    const dependencies = await getDeps.GetAllDependencies(packageName)
-        .then(data => data)
-        .catch(error => console.log(error.message));
-
-    const response = JSON.stringify(dependencies);
-    res.write(response);
-    res.end();
+    await getDeps.GetAllDependencies(packageName)
+        .then(data => {
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            });
+            const response = JSON.stringify(data);
+            res.write(response);
+            res.end();
+        })
+        .catch(error => {
+            res.writeHead(400, {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            });
+            console.log(error);
+            const response = JSON.stringify({error: error.message});
+            res.write(response);
+            res.end();
+        });
 });
 
 server.listen(5000);
