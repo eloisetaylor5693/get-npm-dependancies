2c7c520d4bd803086d52542574d289974c337d5f
Eloise Taylor <rockspud5693@gmail.com>
2021-03-15 13:01:52 +0000

    Fixed writing of sub depenecies, and logging error when response from server fails

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,23 +1,26 @@
 fetch("http://localhost:5000" + window.location.search)
   .then(response => response.json())
+  .then(response => {
+    if (response.error){
+      throw new Error(response.error)
+    }
+
+    return response;
+  })
   .then(packageInfo => {
     const rootContainer = document.getElementById('tree-container');
     addBranchWithDependencies(packageInfo.package, rootContainer, 0);
-
-    console.log({packageInfo:packageInfo});
 
     packageInfo.dependencies.map(package => {
       const level0Container = document.getElementById(`0-${packageInfo.package}-dependencies`);
       addBranchWithDependencies(package.package, level0Container, 1);
 
-      console.log({package:package});
-
-      package.dependencies.map(x =>{
-        const level1Container = document.getElementById(`1-${x.package}-dependencies`);
-        addBranch(x.package, level1Container);
+      package.dependencies.map(parent => {
+        const level1Container = document.getElementById(`1-${package.package}-dependencies`);
+        parent.map(child => addBranch(child, level1Container));
       })
     });
   })
   .catch(error => {
-    writeErrorMessage(error.message);
+    writeErrorMessage(error);
   });
