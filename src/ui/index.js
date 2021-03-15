9100a8360e9abebc29e649642801b3687c4a6255
Eloise Taylor <rockspud5693@gmail.com>
2021-03-15 14:20:19 +0000

    caching api call

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:5000" + window.location.search)
+fetch("http://localhost:5000" + window.location.search, { cache: "force-cache" })
   .then(response => response.json())
   .then(response => {
     if (response.error){
