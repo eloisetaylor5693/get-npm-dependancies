464212399dacf94b1a13e8fecab097b21c38906c
Eloise Taylor <rockspud5693@gmail.com>
2021-03-15 13:05:24 +0000

    Extracted headers into var, and structured error message properly

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,20 @@
 const http = require('http');
 const getDeps = require('./get-npm-dependencies');
 
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+};
+
 const server = http.createServer(async function (req, res) {
 
     console.log(req.url);
     const packageName = req.url.replace('/?package=', '');
 
     if (packageName === '/') {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.write('Please give an npm package in the url');
+        res.writeHead(200, headers);
+        const response = JSON.stringify({ error: 'Please give an npm package in the url' });
+        res.write(response);
         res.end();
 
         return;
@@ -17,21 +23,14 @@ const server = http.createServer(async function (req, res) {
 
     await getDeps.GetAllDependencies(packageName)
         .then(data => {
-            res.writeHead(200, {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            });
+            res.writeHead(200, headers);
             const response = JSON.stringify(data);
             res.write(response);
             res.end();
         })
         .catch(error => {
-            res.writeHead(400, {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            });
-            console.log(error);
-            const response = JSON.stringify({error: error.message});
+            res.writeHead(400, headers);
+            const response = JSON.stringify({ error: error.message });
             res.write(response);
             res.end();
         });
